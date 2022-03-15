9b933e18bdb49a3a5f7a1cc2f1d800f8f5383429
Rewriter00x <colossus00x@icloud.com>
2022-03-15 13:24:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyIsaac
+
+In attempts to learn ue4 I tried to reacreate The Binding of Isaac by Edmund McMillen
---
+++ Source/MyIsaac/MyIsaacGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyIsaacGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYISAAC_API AMyIsaacGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
