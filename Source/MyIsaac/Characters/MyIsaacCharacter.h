b09001ee0fa21914653bfb2f9e22c536d085b4c5
Rewriter00x <colossus00x@icloud.com>
2022-03-19 15:55:42 +0200

    Create MyIsaacCharacter

---
+++ Source/MyIsaac/Characters/MyIsaacCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyPlayableCharacter.h"
+#include "MyIsaacCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYISAAC_API AMyIsaacCharacter : public AMyPlayableCharacter
+{
+	GENERATED_BODY()
+	
+};
